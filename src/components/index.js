f9343f1fd82193ba14242f15f6aeef46d67d700c
n-hassan01 <naimul.hassan.se@gmail.com>
2024-10-03 14:16:24 +0600

    how to add a component of a page

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
-export { default as Navbar } from './Navbar';
-export { default as Main } from './main';
-export { default as Product } from './Products';
-export { default as Footer } from './Footer';
+export { default as Footer } from "./Footer";
+export { default as NavClone } from "./NavClone";
+export { default as Navbar } from "./Navbar";
+export { default as Product } from "./Products";
+export { default as Main } from "./main";
+
